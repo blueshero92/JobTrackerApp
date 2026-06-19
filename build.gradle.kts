84b3bdb6f5a3b23ab23af6dff1ff0cd8fd19709e
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2026-06-19 21:09:33 +0300

    Switched build system to Gradle for improved build flexibility.
    Signed-off-by: blueshero92 <133801686+blueshero92@users.noreply.github.com>

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+	id("org.springframework.boot") version "3.3.5"
+	id("io.spring.dependency-management") version "1.1.6"
+	kotlin("jvm") version "1.9.22"
+	kotlin("plugin.spring") version "1.9.22"
+}
+
+group = "com.deyan"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion.set(JavaLanguageVersion.of(21))
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
