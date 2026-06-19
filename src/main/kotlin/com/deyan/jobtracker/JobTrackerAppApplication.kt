91cc70c0b7d76df7d79b2d14dd900c23783bd1b8
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2026-06-19 20:09:01 +0300

    Intital SpringBoot Kotlin project setup.
    Signed-off-by: blueshero92 <133801686+blueshero92@users.noreply.github.com>

---
+++ src/main/kotlin/com/deyan/jobtracker/JobTrackerAppApplication.kt
@@ -0,0 +1,11 @@
+package com.deyan.jobtracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JobTrackerAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<JobTrackerAppApplication>(*args)
+}
---
+++ src/test/kotlin/com/deyan/jobtracker/JobTrackerAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.deyan.jobtracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JobTrackerAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
